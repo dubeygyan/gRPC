16bcc9ea4fbd26a047997ce931f7cda7cb2db693
Dubey, Gyan Deepak <Gyan.Dubey@pearson.com>
2020-10-17 20:36:16 -0700

    initial setup

---
+++ src/main/java/com/github/dubeygyan/grpc/hello/server/HelloServer.java
@@ -0,0 +1,8 @@
+package com.github.dubeygyan.grpc.hello.server;
+
+public class HelloServer {
+    public static void main(String[] args) {
+        // Just a line to chek everything is correct
+        System.out.println("Hello gRPC");
+    }
+}
